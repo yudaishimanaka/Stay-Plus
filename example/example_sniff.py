adeb72352726cdda707baebdf3131d0b71667118
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-11-13 22:13:50 +0900

    fixed sniff.py

--- example/example_sniff.py
+++ example/example_sniff.py
@@ -1,18 +1,16 @@
 from scapy.all import *
 
 
-counter = 0
 def http_header(pkt):
     http_packet = str(pkt)
-    if http_packet.find('TCP'):
-        if http_packet.find('HTTP'):
-            if http_packet.find('GET'):
-                return get_url(pkt)
+    if http_packet.find('GET'):
+        return get_url(pkt)
 
 
 def get_url(pkt):
-    global counter
-    counter += 1
-    return 'Packet #{}: {} ==> {}'.format(counter, pkt[0][1].src, pkt[0][1].dst)
+    ret = "---------------------------------------TCP PACKET----------------------------------------------------\n"
+    ret += "\n".join(pkt.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
+    ret += "----------------------------------------------------------------------------------------------------\n"
+    return ret
 
 sniff(iface="enp0s25", filter="tcp and port 80", prn=get_url)

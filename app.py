361369e52632f5ee9d981bba519a34141cd93aa2
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-11-23 02:54:18 +0900

    added relation of UI

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run()

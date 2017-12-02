38015e50f673a14ec16177ac58531a3125210e19
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-12-02 18:45:59 +0900

    completed function get_mac

--- stay_status.py
+++ stay_status.py
@@ -3,7 +3,7 @@ from models import User
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
-from scapy.all import *
+from queue import Queue
 import time
 import subprocess
 import re
@@ -12,19 +12,26 @@ import threading
 
 ip_fmt = "192.168.1.%d"
 mac_list = []
+lock = threading.Lock()
+
 
 def get_mac(ip_address):
     output = subprocess.Popen(['arping', '-c', '1', ip_address], stdout=subprocess.PIPE).communicate()
     pattern = r"(\[(.+)\])"
     try:
         match_result = re.split(pattern, output[0].decode('utf-8'))
-        return match_result[2]
+        with lock:
+            if match_result is not None:
+                print(match_result[2])
+            else:
+                pass
     except IndexError:
         pass
 
 
-def get_stay_status(arp_mac, db_mac):
-    return "state"
+def dummy_func(num):
+    return num
+
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -35,9 +42,24 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         if message == 'hello':
-            user = Ss.query(User)
-            for i in range(user.count()):
-                print(user[i].mac_address)
+            def threader():
+                while True:
+                    worker = q.get()
+                    get_mac(worker)
+                    q.task_done()
+
+            q = Queue()
+
+            for x in range(255):
+                t = threading.Thread(target=threader)
+                t.daemon = True
+                t.start()
+
+            for i in range(1, 255):
+                ip = ip_fmt % i
+                q.put(ip)
+
+            q.join()
 
     def on_close(self):
         print("### connection closed ###")

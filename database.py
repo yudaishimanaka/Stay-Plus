03b6eceffb75bf004291ae08375c026470153d91
yudaishimanaka <gurutaminn.h8@gmail.com>
2017-12-17 05:38:08 +0900

    fixed sqlalchemy connection pool_cycle

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import *
+from sqlalchemy import create_engine
 
 
 url = 'mysql+pymysql://root:gurutaminn1009@localhost/stay_and_analyzation?charset=utf8'
-engine = create_engine(url)
+engine = create_engine(url, pool_recycle=14400)
 Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 metadata = MetaData(engine)
 Base = declarative_base()

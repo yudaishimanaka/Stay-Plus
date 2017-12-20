1aa974af2d824f67ec45d5d823709b3f99fdb9fb
URANO Ryoya <urano.works.mail@gmail.com>
2017-12-20 18:44:16 +0900

    DBの設定をconfファイルに分離した

--- database.py
+++ database.py
@@ -1,10 +1,48 @@
+import configparser
+import pathlib
+
 from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import *
 from sqlalchemy import create_engine
 
 
-url = 'mysql+pymysql://root:gurutaminn1009@localhost/stay_and_analyzation?charset=utf8'
+class DBConfig:
+    def __init__(self, config):
+        try:
+            self.host = config['DATABASE']['host']
+            self.user = config['DATABASE']['user']
+            self.password = config['DATABASE']['password']
+            self.dbname = config['DATABASE']['dbname']
+        except KeyError as err:
+            DBConfigError('Not found config: %s' % str(err))
+
+
+class Error(Exception):
+    """Base class of Error."""
+    pass
+
+
+class DBConfigError(Error):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+config_file_path = pathlib.Path('config/database.conf')
+config = configparser.ConfigParser()
+if not config.read(config_file_path):
+    raise DBConfigError('Not found config file: %s' % str(config_file_path.absolute()))
+
+dbconf = DBConfig(config)
+url = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % ( # python>=3.6ならフォーマット文字列使ったほうがいいかも
+    dbconf.user,
+    dbconf.password,
+    dbconf.host,
+    dbconf.dbname,
+)
 engine = create_engine(url, pool_recycle=14400)
 Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 metadata = MetaData(engine)
